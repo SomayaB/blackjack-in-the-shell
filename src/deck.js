2ae8a5827b9e91a60499d296ef90d1a06ec06079
rover <tdunn1248@gmail.com>
2017-05-09 14:18:14 -0700

    creates Deck class

---
+++ src/deck.js
@@ -0,0 +1,14 @@
+const Card = require('./card')
+
+class Deck {
+  constructor(card) {
+    //this.card = card
+  }
+  // create an array of unique cards
+  // 
+  //  returns all cards
+
+  // shuffle deck method {
+  //  returns shuffled deck
+  // }
+}
