163fc4e9893d0cb8b40dc2d5a4854aca19c475bf
rover <tdunn1248@gmail.com>
2017-05-09 16:43:37 -0700

    create deck and card methods

--- src/card.js
+++ src/card.js
@@ -1,10 +1,18 @@
 class Card {
-  constructor(rank, suit, point){
+  constructor(rank, suit){
     this.rank = rank
     this.suit = suit
-    this.point = point
+    this.point = null
   }
-  
+
+  assignPoint( rank ) {
+    if (this.rank === 'ace') {
+          this.point === 11
+    } else if (isNaN(this.rank)) {
+       this.point = 10
+    } else
+        this.rank === this.point
+  }
 }
 
 module.exports = Card
--- src/deck.js
+++ src/deck.js
@@ -1,14 +1,28 @@
 const Card = require('./card')
+const _ = require('lodash')
+// const col = require('colors')
 
 class Deck {
-  constructor(card) {
-    //this.card = card
-  }
-  // create an array of unique cards
-  // 
-  //  returns all cards
 
-  // shuffle deck method {
-  //  returns shuffled deck
-  // }
+  constructor() {
+    this.deckStack = []
+  }
+
+  createDeck() {
+    const heart = col.bgWhite(‘❤️ ‘)
+    const spade = col.bgWhite('♠️')
+    const diamond = col.bgWhite(‘♦️‘)
+    const club = col.bgWhite(‘♣️‘)
+    const suits = [heart,spade,diamond,club]
+    const ranks = ['ace',2,3,4,5,6,7,8,9,10,'jack','queen','king']
+    suits.forEach( (suit) => {
+      ranks.forEach( (rank) => {
+        this.deckStack.push(new Card(suit, rank))
+      })
+    })
+  }
+
+  shuffleDeck() {
+    this.deckStack = _.shuffle(deckStack)
+  }
 }
